67d422ba1cfaf36e1924506efca7ce6081e9a6b0
Cayenne Geis <cayennes@gmail.com>
2015-04-17 13:28:48 -0400

    Rework signals
    Fixes a bug that would add a bit of time when pausing/unpausing
    
    Also added lots of type annotations

--- Timer.elm
+++ Timer.elm
@@ -12,16 +12,17 @@ type alias Model =
     { total:Time.Time
     , elapsed:Time.Time
     , running:Bool
-    } 
+    }
 
 -- UPDATE
 
-update : (Time.Time, Bool) -> Model -> Model
-update (delta, running) model =
-    { total = model.total
-    , elapsed = model.elapsed + delta
-    , running = running
-    }
+type Update = TimeDelta Time.Time | TimerState Bool
+
+update : Update -> Model -> Model
+update action model =
+    case action of
+        TimeDelta elapsed -> { model | elapsed <- model.elapsed + elapsed }
+        TimerState on -> { model | running <- on }
 
 -- VIEW
 
@@ -29,6 +30,7 @@ faceRadius = 120
 markerRadius = 10
 totalDiameter = 2 * (faceRadius + markerRadius)
 
+marker : Model -> Collage.Form
 marker state =
     let fractionElapsed = state.elapsed / state.total
         angle = turns fractionElapsed
@@ -38,22 +40,33 @@ marker state =
        |> Collage.filled color
        |> Collage.move location
 
+face : Model -> Element.Element
 face state =
     Collage.collage totalDiameter totalDiameter
         [ Collage.filled Color.lightBlue (Collage.circle faceRadius)
         , marker state
         ]
 
+view : Model -> Element.Element
 view state = 
     Element.flow Element.down
         [ face state
         , Text.asText state
         ]
 
+main : Signal Element.Element
 main =
     Signal.map view model
 
 -- SIGNALS
+
+timerUpdate : Signal Update
+timerUpdate =
+    let timerRunning = Signal.foldp xor False Keyboard.space
+        timePassed = Time.fpsWhen 60 timerRunning
+    in Signal.merge
+       (Signal.map TimerState timerRunning)
+       (Signal.map TimeDelta timePassed)
 
 initialState : Model
 initialState =
@@ -65,14 +78,3 @@ initialState =
 model : Signal Model
 model =
     Signal.foldp update initialState timerUpdate
-
-timerUpdate : Signal (Time.Time, Bool)
-timerUpdate =
-    Signal.map2
-        (,)
-        (Time.fpsWhen 60 timerRunning) -- TODO: does this do the right thing when the other signal happens?
-        timerRunning
-
-timerRunning : Signal Bool
-timerRunning =
-    Signal.foldp xor False Keyboard.space
