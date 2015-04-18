427483694e894a5e5f8ce511412f94ff7574a6d8
Cayenne Geis <cayennes@gmail.com>
2015-04-18 14:27:00 -0400

    Ability to set timer + some refactoring

--- Timer.elm
+++ Timer.elm
@@ -1,53 +1,97 @@
 import Color
-import Graphics.Element as Element
+import Debug
 import Graphics.Collage as Collage
+import Graphics.Element as Element
+import Graphics.Input.Field as Field
 import Keyboard
+import Result
 import Signal
+import String
 import Text
 import Time
 import Window
 
+main : Signal Element.Element
+main =
+    Signal.map2 view Window.dimensions model
+
 -- MODEL
 
 type alias Model =
-    { total:Time.Time
+    { setTime:Field.Content
     , elapsed:Time.Time
     , running:Bool
     }
 
+isFinished : Model -> Bool
+isFinished state =
+    state.elapsed > total state
+
+total : Model -> Time.Time
+total { setTime } =
+    case String.toFloat setTime.string of
+        Result.Ok i -> i * Time.minute
+        Result.Err e -> 24 * Time.hour
+
 -- UPDATE
 
-type Update = TimeDelta Time.Time | TimerState Bool
+type Update = TimeDelta Time.Time | TimerState Bool | SetTime Field.Content
 
 update : Update -> Model -> Model
 update action model =
     case action of
         TimeDelta elapsed -> { model | elapsed <- model.elapsed + elapsed }
         TimerState on -> { model | running <- on }
+        SetTime newTime -> { model | setTime <- newTime }
+
+-- Signals
+
+model : Signal Model
+model =
+    let initialState =
+        { setTime =
+            { string = "4"
+            , selection =
+                { start = 1
+                , end = 1
+                , direction = Field.Forward
+                }
+            }
+        , elapsed = 0
+        , running = False
+        }
+    in Signal.foldp update initialState updates
+
+updates : Signal Update
+updates =
+    let timerRunning = Signal.foldp xor False Keyboard.space
+        timePassed = Time.fpsWhen 30 timerRunning
+        setTime = Signal.subscribe setTimeUpdates
+    in Signal.mergeMany
+       [ (Signal.map TimerState timerRunning)
+       , (Signal.map TimeDelta timePassed)
+       , (Signal.map SetTime setTime)
+       ]
+
+setTimeUpdates : Signal.Channel Field.Content
+setTimeUpdates = Signal.channel Field.noContent
 
 -- VIEW
 
-faceRadius = 120
-markerRadius = 20
-totalDiameter = 2 * (faceRadius + markerRadius)
+view : (Int, Int) -> Model -> Element.Element
+view (width, height) state =
+    Element.flow Element.right
+        [ timer (((width * 2) // 3), height) state
+        , settings ((width // 3), height) state
+        ]
 
-finished state =
-    state.elapsed > state.total
+-- timer
 
-marker : Model -> Collage.Form
-marker state =
-    let fractionElapsed = state.elapsed / state.total
-        locationAngle = (degrees 90) - (turns fractionElapsed)
-        location = fromPolar (faceRadius, locationAngle)
-        markerAngle = (degrees 270) - (turns fractionElapsed)
-        color = if finished state then base2 else base02
-        shape = if state.running
-                   then Collage.ngon 3 markerRadius
-                   else Collage.ngon 6 (markerRadius / 2)
-    in shape
-       |> Collage.filled color
-       |> Collage.rotate markerAngle
-       |> Collage.move location
+timer : (Int, Int) -> Model -> Element.Element
+timer (width, height) state =
+    let backgroundColor = if isFinished state then base02 else base2
+    in Element.container width height Element.middle (face state)
+       |> Element.color backgroundColor
 
 face : Model -> Element.Element
 face state =
@@ -56,45 +100,61 @@ face state =
         , marker state
         ]
 
-view : (Int, Int) -> Model -> Element.Element
-view (width, height) state =
-    let backgroundColor = if finished state then base02 else base2
-    in Element.container width height Element.middle (face state)
+marker : Model -> Collage.Form
+marker state =
+    let fractionElapsed = state.elapsed / total state
+        locationAngle = (degrees 90) - (turns fractionElapsed)
+        location = fromPolar (faceRadius, locationAngle)
+        markerAngle = (degrees 270) - (turns fractionElapsed)
+        color = if isFinished state then base2 else base02
+        shape = if state.running
+                   then Collage.ngon 3 markerRadius
+                   else Collage.ngon 6 (markerRadius / 2)
+    in shape
+       |> Collage.filled color
+       |> Collage.rotate markerAngle
+       |> Collage.move location
+
+-- settings
+
+settings : (Int, Int) -> Model -> Element.Element
+settings (width, height) state =
+    let finished = isFinished state
+        backgroundColor = if finished then base03 else base3
+        textStyle =
+            Text.Style
+                [ "Source Code Pro" ]
+                (Just 16)
+                (if finished then base00 else base0)
+                False
+                False
+                Nothing
+        fieldStyle =
+            Field.Style
+                (Field.uniformly 4)
+                (Field.Outline blue (Field.uniformly 4) 0)
+                Field.noHighlight
+                textStyle
+    in state.setTime
+       |> Field.field fieldStyle (Signal.send setTimeUpdates) "set timer"
+       |> Element.color backgroundColor
+       |> Element.container width height Element.middle 
        |> Element.color backgroundColor
 
-main : Signal Element.Element
-main =
-    Signal.map2 view Window.dimensions model
+-- Sizes
 
--- SIGNALS
+faceRadius = 120
+markerRadius = 20
+totalDiameter = 2 * (faceRadius + markerRadius)
 
-timerUpdate : Signal Update
-timerUpdate =
-    let timerRunning = Signal.foldp xor False Keyboard.space
-        timePassed = Time.fpsWhen 60 timerRunning
-    in Signal.merge
-       (Signal.map TimerState timerRunning)
-       (Signal.map TimeDelta timePassed)
-
-initialState : Model
-initialState =
-    { total = 30 * Time.second
-    , elapsed = 0 * Time.millisecond
-    , running = False
-    }
-
-model : Signal Model
-model =
-    Signal.foldp update initialState timerUpdate
-
--- COLORS http://ethanschoonover.com/solarized
+-- Colors http://ethanschoonover.com/solarized
 
 base03 = Color.rgb 00 43 54 -- dark primary background
 base02 = Color.rgb 7 54 66 -- dark alternate background
-base01 = Color.rgb 88 110 117 -- light alternate text
+base01 = Color.rgb 88 110 117 -- light alternate text / dark de-emphasized text
 base00 = Color.rgb 101 123 131 -- light primary text
 base0 = Color.rgb 131 148 150 -- dark primary text
-base1 = Color.rgb 147 161 161 -- dark alternate text
+base1 = Color.rgb 147 161 161 -- dark alternate text / light de-emphasized text
 base2 = Color.rgb 238 232 213 -- light alternate background
 base3 = Color.rgb 253 246 227 -- light primary background
 yellow = Color.rgb 181 137 0
